94625c489f064e63240d70bc8c1346d262e08d3d
Elvin Hiruthayam <elvinbca.ngp@gmail.com>
2020-09-10 05:12:48 -0700

    Testing web page

---
+++ static/chola_design/base.js
@@ -0,0 +1,34 @@
+var slideIndex = 1;
+//showSlides(slideIndex);
+timerlides()
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function timerlides() {
+    showSlides(slideIndex);
+    slideIndex+=1;
+    setTimeout(timerlides, 5000); // Change image every 2 seconds
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
